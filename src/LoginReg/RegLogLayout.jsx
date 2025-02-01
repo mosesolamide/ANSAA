5a290aef1ba89bc83fdf2c716e17b8015a26c4c1
mosesolamide <olamidemoses132@gmail.com>
2025-02-01 09:06:09 +0100

    updated
    updated

--- src/LoginReg/RegLogLayout.jsx
+++ src/LoginReg/RegLogLayout.jsx
@@ -3,13 +3,13 @@ import { Outlet,NavLink,Link } from 'react-router-dom'
 
 export default function RegLogLayout() {
     return(
-        <div className="bg-[url('/images/Form.png')] h-screen w-[100vw] bg-cover flex flex-wrap justify-center items-center px-4 gap-6">
+        <div className="bg-[url('/images/Form.png')] w-[100vw] h-[100%] sm:h-[100vh] bg-cover flex flex-wrap justify-center items-center p-4 gap-6">
             <div className='text-white '>
                 <h1 className=' w-[260px] md:w-[350px] font-[600] sm:font-[700] text-[15px] sm:text-1xl lg:text-2xl leading-[20px] sm:leading-[21px]'>Welcome to Anambra Signage & Advertisement Agency(ANSAA)</h1>
                 <p className='font-[500] text-[10px] sm:text-sm my-2'>...your progress partner in business</p>
                 <button className='bg-red-700 p-2  text-white text-[10px] sm:text-sm rounded-sm'>Apply for Signage Permit</button>
             </div>
-            <form className='bg-white w-[350px] h-96 sm:h-[70%] rounded-sm flex flex-col items-center p-4'>
+            <form className='bg-white w-[350px] h-[370px] md:h-[380px] rounded-sm flex flex-col items-center p-4'>
                 <img src="images/log.png" alt="logo" className='w-20' />
                 <div>
                     <ul className='flex justify-around w-full text-center'>  
@@ -35,6 +35,7 @@ export default function RegLogLayout() {
                             </NavLink>  
                         </li>  
                     </ul>
+                    <Link to="/" className='text-[9px] underline text-buttonColor'>Back Home</Link>
                 </div>
                 <Outlet />
             </form>
